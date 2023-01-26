15fb3d98a4cbbbc6e99ae070a209a459be1f8532
Daniel Rotaru <daniel_rotaru@hotmail.com>
2023-01-26 10:10:50 +0100

    Added img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,12 @@ const Header = () => {
     return (
         <nav className="main-header">
             <ul>
-                <li><a href="https://www.google.se">Länk 1</a></li>
-                <li><a href="https://www.google.se">Länk 1</a></li>
-                <li><a href="https://www.google.se">Länk 1</a></li>
-                <li><a href="https://www.google.se">Länk 1</a></li>
+                <li><a href="https://www.google.se">People</a></li>
+                <li><a href="https://www.google.se">Planets</a></li>
+                <li><a href="https://www.google.se">Films</a></li>
+                <li><a href="https://www.google.se">Species</a></li>
+                <li><a href="https://www.google.se">Vehicles</a></li>
+                <li><a href="https://www.google.se">Starships</a></li>
             </ul>
         </nav>
     )
---
+++ src/views/Planets/Panets.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Planets = () => {
+    return (
+        <div>
+            <div className="content">
+
+            </div>
+            <div className="sidebar">
+
+            </div>
+        </div>
+    )
+}
+
+export default Planets;

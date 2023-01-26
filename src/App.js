1c7c3e95aa97ce176dc365f36856a5448d0d4386
Lucas Magnusson <lucasmyeet@gmail.com>
2023-01-26 10:17:37 +0100

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  
+  useEffect(() => {
+    async function fetchPeople() {
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = await res.json();
+      setPeople(data.results)
+    }
 
-  const [counter, setCounter] = useState(0);
+    async function fetchPlanets() {
+      let res = await fetch('https://swapi.dev/api/planets/?format=json');
+      let data = await res.json();
+      setPlanets(data.results)
+    }
 
-  const onBtnClickHandler = () => {
-    setCounter(counter+1);
-  }
+    fetchPeople();
+    fetchPlanets();
+
+  }, [])
+  console.log('people', people);
+  console.log('planets', planets);
+
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <Header />
-            <button onClick={onBtnClickHandler} type="button" className="btn btn-primary">Current Number: {counter}</button>
-          <Footer />
-        </div>
-      </div>
     </div>
   );
 }

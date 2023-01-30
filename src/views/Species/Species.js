5248c6a23c6662a911f66a3a3bea8a374fe4d4e5
Lucas Magnusson <96703310+LuckyyLukee@users.noreply.github.com>
2023-01-30 10:17:39 +0100

    Modal test

--- src/views/Species/Species.js
+++ src/views/Species/Species.js
@@ -1,38 +1,78 @@
+import React, { useState } from 'react';
 import "./species.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { Popover, OverlayTrigger, ButtonToolbar, Button } from "react-bootstrap";
+import { Popover, OverlayTrigger, ButtonToolbar, Button, Modal } from "react-bootstrap";
+
+// const Species = ({data}) => {
+//     console.log(data);
+//     return (
+//         <div className="species-container">
+//         {data.results.map((species) => {
+//             const popoverRight = (
+//                 <Popover id="popover" title="Popover right">
+//                     <p>{species.name}</p>
+//                     <ul>
+//                         <li>classification: {species.classification}</li>    
+//                     </ul>
+//                 </Popover>
+//             )
+//             return (
+//                 <ButtonToolbar>
+//                     <OverlayTrigger trigger="focus" placement="top" overlay={popoverRight}>
+//                         <Button class="btn btn-transparent">
+//                             <div className="species-info">
+//                                 <div className="text-container">
+//                                     <p>Species name: {species.name}</p>
+//                                     <p>homeworld: {species.homeworld}</p>
+//                                 </div>
+//                             </div>
+//                         </Button>
+//                     </OverlayTrigger>
+//                 </ButtonToolbar>
+//             )
+//         })}
+//         </div>
+//     )
+// } 
 
 const Species = ({data}) => {
+    const [show, setShow] = useState(false)
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     console.log(data);
     return (
         <div className="species-container">
         {data.results.map((species) => {
-            const popoverRight = (
-                <Popover id="popover" title="Popover right">
-                    <p>{species.name}</p>
-                    <ul>
-                        <li>classification: {species.classification}</li>    
-                    </ul>
-                </Popover>
-            )
             return (
-                <ButtonToolbar>
-                    <OverlayTrigger trigger="focus" placement="top" overlay={popoverRight}>
-                        <Button>
-                            <div className="species-info">
-                                <div className="text-container">
-                                    <p>Species name: {species.name}</p>
-                                    <p>homeworld: {species.homeworld}</p>
-                                </div>
+                <>
+                    <Button variant="primary" onClick={handleShow}>
+                        <div className="species-info">
+                            <div className="text-container">
+                                <p>Species name: {species.name}</p>
+                                <p>homeworld: {species.homeworld}</p>
                             </div>
-                        </Button>
-                    </OverlayTrigger>
-                </ButtonToolbar>
+                        </div>
+                    </Button>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>{species.name}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>{species.classification}</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={handleClose}>
+                                Save Changes
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
             )
         })}
         </div>
     )
-} 
+}
 
 export default Species;

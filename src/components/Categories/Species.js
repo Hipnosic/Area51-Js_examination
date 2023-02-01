603987f6b3af73a7c79479a5c1ae755edef5ccc1
Lucas Magnusson <96703310+LuckyyLukee@users.noreply.github.com>
2023-02-01 13:15:52 +0100

    fixed lists

--- src/components/Categories/Species.js
+++ src/components/Categories/Species.js
@@ -3,14 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Card from "../Card/Card";
 
-const Species = ({data}) => {
-    return (
-        <div className="card-container">
-            {data.results.map((species) => {
-                return <Card {...species} />
-            })}
-        </div>
-    )
-} 
+const Species = ({ data }) => {
+  return (
+    <div className="card-container">
+      {data.results.map((specie) => {
+        return <Card {...specie} key={specie.name} />;
+      })}
+    </div>
+  );
+};
 
 export default Species;
